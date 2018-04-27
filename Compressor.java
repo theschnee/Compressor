2f04c54fe5fa8f1d922304e1110cefe317a760e4
theschnee <callum_hafner-schnee@asl.org>
2018-04-27 16:04:34 -0400

    Add files via upload

---
+++ Compressor.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+
+
+
+public class Compressor {
+	private Map 
+	public void compress() {
+		
+	}
+	
+	public void decompress() {
+		
+	}
+	
+	
+}
